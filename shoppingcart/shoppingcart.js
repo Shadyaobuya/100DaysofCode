5aee01a8282b157ec7b2f53e5ce3344dd821838a
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-04-01 08:04:32 +0300

    shoppingcart

---
+++ shoppingcart/shoppingcart.js
@@ -0,0 +1,57 @@
+//a program that creats an object myCart and gives it properties and functionality
+var myCart={                                                //store the items that can be bought in the cart in an array
+    fruits:["mango", "orange", "pineapple","apple"],          //declare property quantity and initialise it to 0
+    quantity:0,                                             //initialise the quantity property to 0
+                                                    
+    addToCart(addedItem){                                   //declare function addToCart to add items in the cart                             //
+
+        totalItems=this.quantity+addedItem;
+
+        console.log("The total items in the cart is "+totalItems);
+    
+    },
+    removeFromCart(removedItem){                          //declare function removeFrom Cart to remove items from cart
+
+        totalItems=totalItems-removedItem;
+        console.log("The total items in the cart is "+totalItems);
+    
+    },
+    calculatePrice(fruitName){                               //declare function to calculate the price of the items in the cart
+        this.fruits.forEach((fruit) => {                  //loop through each item in the array
+            
+            
+            if (fruitName==="mango"){
+               var totalCost=30*totalItems;
+            console.log("The total cost of mangoes bought is "+totalCost);
+            
+         }
+         else if (fruitName==="orange"){
+            totalCost=20*totalItems;
+            console.log("The total cost of oranges bought is "+totalCost);
+        }
+
+        else if (fruitName==="pineapple"){
+           totalCost=50*totalItems;
+          console.log("The total cost of pineapple bought is "+totalCost);
+         }
+        else if(fruitName==="apple"){
+            totalCost=30*totalItems;                                                                                                                                                                                                                                                                                                                                                              
+       
+           console.log("The total cost of apples bought is "+totalCost);
+        }
+        else{
+                   console.log("This item does not exist in the cart");
+               }
+    
+   
+        }
+
+        )}
+    }
+         
+
+myCart.addToCart(6);   //call function
+myCart.removeFromCart(2);
+myCart.calculatePrice("banana");
+
+
